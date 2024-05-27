957c253aa587919d117fba25bb29361b6b8cf130
Prakerja <prakerja@Prakerjas-MacBook-Pro-88.local>
2024-05-27 18:08:07 +0700

    update

---
+++ js/function.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 60) {
+            $('header').addClass("header-fixed");
+        } else {
+            $('header').removeClass("header-fixed");
+        }
+
+        // for scroll-top trigger
+        if (scroll >= 400) {
+            $('.scroll-top').addClass("is-show");
+        } else {
+            $('.scroll-top').removeClass("is-show");
+        }
+
+        // Scroll to top 
+        $(".scroll-top").on("click", function() {
+            $(window).scrollTop(0);
+        });
+
+    });
+});
